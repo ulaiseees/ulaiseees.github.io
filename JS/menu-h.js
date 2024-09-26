3670bc057cb53859e3e12f58c781b8dbe090be97
Ulises Borda <ulisesborda@gmail.com>
2024-09-26 18:36:54 -0300

    Implementación de MediaQuerys y Cambios

--- JS/menu-h.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
-    const menu = document.querySelector('.menu');
-
-    menuToggle.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    });
-});
---
+++ JS/toggle-menu.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const nav = document.querySelector('.barra-navegacion');
+    nav.classList.toggle('active');
+}
